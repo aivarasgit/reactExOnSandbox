9e9cbb0fedd01cdc5587d5ba271b70b547c08497
aivarasgit <69480007+aivarasgit@users.noreply.github.com>
2020-09-28 10:59:56 +0300

    Styling Some style on

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from "react";
 import "./styles.css";
 import MembersDataList from "./components/DataList";
+import EventRegForm from "./components/EventRegForm";
 
 export default function App() {
+  const [name, input] = "";
+  const [values, valuesChanged] = EventRegForm(name, input);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <MembersDataList name="Akivaraki" surname="nobody" isActive={false} />
-      <MembersDataList name="Loli" surname="Namatamoli" isActive={true} />
-      <MembersDataList name="Kyiuosaki" surname="Abaravfeki" isActive={false} />
+      <form className="RegistrationForm">
+        <label for="nameID">Name:</label>
+        <input
+          name="name"
+          type="text"
+          id="nameID"
+          value={values.name}
+          onChange={valuesChanged}
+        />
+        <label for="inputID">Input</label>
+        <input
+          name="input"
+          type="text"
+          id="inputID"
+          value={values.input}
+          onChange={valuesChanged}
+        />
+
+        <MembersDataList name="Akivaraki" surname="nobody" isActive={false} />
+        <MembersDataList name="Loli" surname="Namatamoli" isActive={true} />
+        <MembersDataList
+          name="Kyiuosaki"
+          surname="Abaravfeki"
+          isActive={false}
+        />
+      </form>
     </div>
   );
 }
---
+++ src/components/EventRegForm.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const EventRegForm = (...formValues) => {
+  const [values, setValues] = useState(formValues);
+  return [
+    values,
+    (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value + "changed" });
+    }
+  ];
+};
+
+export default EventRegForm;
